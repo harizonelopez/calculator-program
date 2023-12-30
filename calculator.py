f08c0f7c602f99c7c98e67cc66cde13ef0dabf9e
HARISON OTIENO <136503275+harizonelopez@users.noreply.github.com>
2023-12-30 12:37:50 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,93 @@
+#@Dev_Aladinh production
+#calculator app
+
+
+def status():
+    a = "+"
+    b = "-"
+    c = "*"
+    d = "/"
+    
+    print("\nWhich operation do you wannah do.>a{+}, b{-}, c{*}, d{/}")
+
+    operation = input(":) ").lower()
+
+    if operation == "a":
+        operation = addition()
+
+    elif operation == "b":
+        operation = subtraction()
+
+    elif operation == "d":
+        operation = division()
+
+    elif operation == "c":
+        operation = multiplication()
+
+    else:
+        print("\n-->The operation couldn't be found<--")
+
+    print("Enter the two numbers you wannah use: ")
+
+
+def addition():
+    number_a = int(input("Num 1:  "))
+    number_b = int(input("Num 2:  "))
+   
+    Answer = number_a + number_b
+    print("The answer is-->" + str(Answer))
+
+def subtraction():
+    number_a = int(input("Num 1:  "))
+    number_b = int(input("Num 2:  "))
+
+    Answer = number_a - number_b
+    print("The answer is-->" + str(Answer))
+
+def division():
+    number_a = int(input("Num 1:  "))
+    number_b = int(input("Num 2:  "))
+
+    Answer = number_a / number_b
+    print("The answer is-->" + str(Answer))
+
+
+def multiplication():
+    number_a = int(input("Num 1:  "))
+    number_b = int(input("Num 2:  "))
+
+    Answer = number_a * number_b
+    print("The answer is-->" + str(Answer))
+
+
+def calculator():
+    print("\nHelloh user ^!^!^\nEnter your name.")
+    identity = input(":) ").lower()
+
+    print("\nWelcome " + identity + " to my simple calculator app\nNOTE:YOU CAN ONLY INPUT TWO NUMBERS\n\nDo you wannah play?{yes / no}")
+    play= input(":) ").lower()
+
+    if play in ["yes", "yeap", "yeap", "ok", "okay"]:
+        play = status()
+    else:
+        print("\nIt was a nice time with you, welcome again next time^:^")
+        quit()
+
+calculator()
+
+
+def next_play():
+    print("\nDo you wannah play again? {yes or no}")
+    play_again = input(":) ").lower()
+
+    if play_again == "yes":
+        play_again = status()
+    
+    else:
+        print("Welcome " + identity + " next time to the play ^.^")
+
+next_play()
+        
+
+
+
